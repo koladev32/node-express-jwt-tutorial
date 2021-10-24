125c9109bc905618aff5d1750cd5357fbd13413b
Kolawole Mangabo <onaelmangabo@gmail.com>
2021-10-24 14:29:57 +0100

    - feat: add user model

---
+++ src/models/user.ts
@@ -0,0 +1,24 @@
+import { IUser } from "../types/user";
+import { model, Schema } from "mongoose";
+
+const userSchema: Schema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false
+    },
+  },
+  { timestamps: true }
+);
+
+export default model<IUser>("user", userSchema);
